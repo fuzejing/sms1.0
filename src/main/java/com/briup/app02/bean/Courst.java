746bf3f9dc64c3641dcd80b4278e48d1da0f5844
fuzejing <fuzejing@FU>
2018-07-13 16:07:33 +0800

    第一天

---
+++ src/main/java/com/briup/app02/bean/Courst.java
@@ -0,0 +1,14 @@
+package com.briup.app02.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Courst {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
